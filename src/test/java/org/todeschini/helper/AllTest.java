5c929bfb027a4f4fd351a4614e9ab1007e1770b9
Artur Todeschini <artodeschini@gmail.com>
2018-06-09 23:19:12 -0300

    create a suit of many class tests

---
+++ src/test/java/org/todeschini/helper/AllTest.java
@@ -0,0 +1,9 @@
+package org.todeschini.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { ArraysCompareTest.class, StringHelperTest.class, QuickBeforeAfterTest.class } )
+public class AllTest {
+}
